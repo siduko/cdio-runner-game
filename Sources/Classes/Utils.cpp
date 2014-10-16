06f211bbb11208721860a578701e7b32388ee3b1
siduko <ace.siduko@gmail.com>
2014-10-16 21:31:08 +0700

    remove old classes and resources

--- Sources/Classes/Utils.cpp
+++
@@ -1,9 +0,0 @@
-#include "Utils.h"
-
-
-float Utils::randomValueBetween(float low, float high){
-	return (((float)rand() / RAND_MAX) * (high - low)) + low;
-}
-int Utils::randomValueBetween(int low, int high){
-	return low + (rand() % high);
-}
